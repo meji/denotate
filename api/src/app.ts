3be1e6bc74691531d23c012576f8cccf3f7b9cfa
meji <soy@meji.es>
2020-11-14 01:48:08 +0100

    Devuelvo los posts de una categoría con Promise.all

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,5 +1,6 @@
 import { Application, Router } from '../deps.ts'
 import posts from './routes/posts.ts'
+import categories from './routes/categories.ts'
 
 export const app = new Application()
 const router = new Router()
@@ -7,5 +8,8 @@ const router = new Router()
 router.get('/', ctx => {
   ctx.response.body = 'Hola index'
 })
-app.use(router.routes()).use(posts.routes())
+app
+  .use(router.routes())
+  .use(posts.routes())
+  .use(categories.routes())
 app.use(router.allowedMethods())
