26abe76980ae6e1ab285e14e8eb4875629831d0b
meji <soy@meji.es>
2020-12-04 13:50:07 +0100

    Configuraciones de puerto

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 import { app } from "./src/app.ts";
 import env from "./src/config/env.ts";
 import { displayDinosaur } from "./src/utils/index.ts";
+import { flags } from "./deps.ts";
 displayDinosaur(env.denoEnv === "PROD");
 
-await app.listen(`${env.denoHost}:${env.denoPort}`);
+const { args, exit } = Deno;
+
+const DEFAULT_PORT = env.denoPort;
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : Number(DEFAULT_PORT);
+if (isNaN(port)) {
+  console.log("This is not port number");
+  exit(1);
+}
+await app.listen({ port: port });
