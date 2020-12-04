99080928cd44c3a7c5584a15da9d9c2476eeaccd
meji <soy@meji.es>
2020-12-04 13:34:18 +0100

    Cambio port a env para probar en heeroku

--- main.ts
+++ main.ts
@@ -1,10 +1,6 @@
 import { app } from "./src/app.ts";
 import env from "./src/config/env.ts";
 import { displayDinosaur } from "./src/utils/index.ts";
-import { parse } from "./deps.ts";
 displayDinosaur(env.denoEnv === "PROD");
 
-const { args } = Deno;
-const argPort = parse(args).port;
-
-await app.listen(`${env.denoHost}:${argPort ? Number(argPort) : env.denoPort}`);
+await app.listen(`${env.denoHost}:${env.denoPort}`);
