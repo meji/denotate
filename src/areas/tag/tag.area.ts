3d2e4d7d90dfe04b299ed7577a82ced622f94427
meji <soy@meji.es>
2020-12-30 19:43:17 +0100

    Elimino el area de tag ya uqe lo hacemos desde posts

--- src/areas/tag/tag.area.ts
+++
@@ -1,8 +0,0 @@
-import { Area } from "../../../deps.ts";
-import { TagController } from "./tag.controller.ts";
-
-@Area({
-  baseRoute: "/tags",
-  controllers: [TagController]
-})
-export class TagArea {}
